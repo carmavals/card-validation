d8bdc582cedf3477ada96c9fa989e4541998bdb1
carmavals <mvcarbajals@pucp.edu.pe>
2022-09-22 13:00:52 -0500

    aun falta que el valor de numero se enmascare y que las funciones de enter y el boton funcionen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import validator from './validator.js';
+//accediendo al input text
 
+
+let valorDom = document.getElementById('numero')
+let numero = valorDom.value
+
+valorDom.innerHTML = validator.maskify(numero)
+
+let input = document.getElementById('numero')
+input.addEventListener('keypress', function (evento_Enter) {
+    if (evento_Enter.key === 'Enter') {
+        evento_Enter.preventDefault()
+        document.getElementById('submit').click()
+    }
+})
+
+if (validator.isValid(numero) === true) {
+    let tarjeta_valida = document.createElement('div')
+    tarjeta_valida.setAttribute('div', 'tarjetaValida')
+    let div_tarjeta = document.querySelector('.tarjeta')
+    div_tarjeta.appendChild(tarjeta_valida)
+} else {
+    let tarjeta_invalida = document.createElement('div')
+    tarjeta_invalida.setAttribute('div', 'tarjetaInvalida')
+    let div_tarjeta = document.querySelector('.tarjeta')
+    div_tarjeta.appendChild(tarjeta_invalida)
+
+}
+
+
+console.log(numero)
+console.log(valorDom)
+console.log(input)
+console.log(evento_Enter)
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,19 @@
 const validator = {
-  // ...
+  isValid(numero_tarjeta) {
+    let digitos = numero_tarjeta.split('').reverse().map(x => parseInt(x))
+    const ultimo_digito = digitos.shift();
+    let suma = digitos.reduce(
+      (acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val *= 2) > 9 ? val - 9 : val)),
+      0
+    );
+    suma += ultimo_digito;
+    return suma % 10 === 0;
+    
+  },
+  maskify(numero_tarjeta) {
+    return numero_tarjeta.split('').map((numero, indice) => indice < numero_tarjeta.length - 4 ? '#' : numero).join('')
+  }
 };
 
 export default validator;
+
